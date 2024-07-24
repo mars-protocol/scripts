08c50ed4231739a7c50361822837ce916b037aa5
piobab <piobab0x@proton.me>
2024-07-24 21:04:37 +0200

    Update fmt and read config from env.

--- alerts/liquidation_checker_telegram.py
+++ alerts/liquidation_checker_telegram.py
@@ -8,6 +8,11 @@ from telegram import Bot
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 
+# Health factor and total debt thresholds
+MIN_HF = float(os.getenv('MIN_HF', 0.2))
+MAX_HF = float(os.getenv('MAX_HF', 1.0))
+MIN_TOTAL_DEBT = float(os.getenv('MIN_TOTAL_DEBT', 1000000))
+
 # Logging configuration
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -25,17 +30,17 @@ def fetch_and_filter_data(url, url_label, min_health_factor, max_health_factor, 
             if min_health_factor < float(item['health_factor']) <= max_health_factor and float(item['total_debt']) > min_total_debt
         ]
         
-        # Format the message to be sent to Telegram
-        message = f"\n### {url_label} ###\n\n"
-        message += f"| {'acc_id':<10} | {'acc_kind':<10} | {'hf':<10} | {'debt':<10} |\n"
-        message += f"| {'-'*10} | {'-'*10} | {'-'*10} | {'-'*10} |\n"
+        # If no data is found, return an empty string
+        if not filtered_data:
+            return ""
         
+        # Format the message to be sent to Telegram
+        message = f"\n### {url_label} ###\n"
         for item in filtered_data:
             acc_id = item['account_id']
-            acc_kind = item['account_kind']
             hf = float(item['health_factor'])
             debt = float(item['total_debt']) / 1000000
-            message += f"| {acc_id:<10} | {acc_kind:<10} | {hf:<10.4f} | {debt:<10.4f} |\n"
+            message += f"- acc_id: {acc_id}, hf: {hf:.4f}, debt: {debt:.4f}\n"
         
         return message
     except requests.RequestException as e:
@@ -51,16 +56,14 @@ async def send_alert():
         'NTRN RB': 'https://api.marsprotocol.io/v1/unhealthy_positions/neutron/redbank'
     }
     
-    # Predefined thresholds
-    min_health_factor = 0.2
-    max_health_factor = 1.0
-    min_total_debt = 1000000
-    
     # Aggregate messages
     full_message = ""
     for label, url in urls.items():
-        full_message += fetch_and_filter_data(url, label, min_health_factor, max_health_factor, min_total_debt) + "\n"
+        message = fetch_and_filter_data(url, label, MIN_HF, MAX_HF, MIN_TOTAL_DEBT)
+        if message:
+            full_message += message + "\n"
     
-    await bot.send_message(chat_id=CHANNEL_ID, text=f"```\n{full_message}\n```", parse_mode='Markdown')
+    if full_message:
+        await bot.send_message(chat_id=CHANNEL_ID, text=f"```\n{full_message}\n```", parse_mode='Markdown')
 
 asyncio.run(send_alert())
